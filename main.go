5500b199d7bbc5ad1271579acd2a7f52ed316d18
lmarchet <lucasmarchetti21@gmail.com>
2024-06-30 22:19:25 +0200

    cleaning and commenting

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
 	"strings"
 	"sync"
 	"time"
@@ -14,7 +13,7 @@ import (
 type Server struct {
 	host    string
 	port    string
-	clients []*Client
+	clients []Client
 	mutex   sync.Mutex
 }
 
@@ -34,8 +33,8 @@ type Logs struct {
 	Message string
 }
 
-func New(config *Config) *Server {
-	return &Server{
+func New(config Config) Server {
+	return Server{
 		host: config.Host,
 		port: config.Port,
 	}
@@ -44,6 +43,7 @@ func New(config *Config) *Server {
 var historic []Logs
 
 func (server *Server) Run() {
+	// Start listening for incoming TCP connections on the specified host and port.
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", server.host, server.port))
 	log.Printf("Listening on port %s", server.port)
 	if err != nil {
@@ -51,44 +51,48 @@ func (server *Server) Run() {
 	}
 	defer listener.Close()
 	for {
+		// Accept incoming connection from listener.
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-
-		client := &Client{
+		// Create a new Client instance for the accepted connection
+		client := Client{
 			conn: conn,
 		}
 		// Welcome message.
 		welcome := PrintWelcome()
 		client.conn.Write([]byte(welcome))
-		// Ask for pseudo and add client to server's list
+		// Ask for pseudo and erase the end of line.
 		client.conn.Write([]byte("[ENTER YOUR PSEUDO]: "))
 		reader := bufio.NewReader(client.conn)
 		pseudo, _ := reader.ReadString('\n')
 		client.pseudo = strings.TrimRight(pseudo, "\n")
-
+		// Lock the mutex to synchronize access to the server's clients list.
 		server.mutex.Lock()
+		// Append the new client to the server's list of clients.
 		server.clients = append(server.clients, client)
+		// Unlock the mutex to allow other goroutines to access the clients list.
 		server.mutex.Unlock()
-
-		// Handle client messages in a goroutine
+		// Handle client messages in a goroutine.
 		go server.handleClient(client)
 	}
 }
 
-func (server *Server) handleClient(client *Client) {
+func (server *Server) handleClient(client Client) {
+	// Host log.
+	log.Printf("Client %s connected\n", client.pseudo)
+	// Notify other clients about new connection
+	server.broadcastMessage(Client{}, fmt.Sprintf("%s has joind the chat...\n", client.pseudo))
 	// Display historic of the conversations.
 	for _, log := range historic {
 		client.conn.Write([]byte("[" + log.Time + "][" + log.Pseudo + "]: " + log.Message))
 	}
-	log.Printf("Client %s connected\n", client.pseudo)
+	// Remove client from server's list when client disconnects.
 	defer func() {
-		// Remove client from server's list when client disconnects
 		server.mutex.Lock()
 		defer server.mutex.Unlock()
-
 		for i, c := range server.clients {
 			if c == client {
 				server.clients = append(server.clients[:i], server.clients[i+1:]...)
@@ -96,38 +100,39 @@ func (server *Server) handleClient(client *Client) {
 			}
 		}
 		client.conn.Close()
-
 	}()
-
+	// Read client input.
 	reader := bufio.NewReader(client.conn)
-
-	// Notify other clients about new connection
-	server.broadcastMessage(nil, fmt.Sprintf("%s has joind the chat...\n", client.pseudo))
-
 	for {
 		message, err := reader.ReadString('\n')
 		if err != nil {
 			log.Printf("Client %s disconnected\n", client.pseudo)
 			// Broadcast disconnect message to remaining clients
-			server.broadcastMessage(nil, fmt.Sprintf("%s has left the chat...\n", client.pseudo))
+			server.broadcastMessage(Client{}, fmt.Sprintf("%s has left the chat...\n", client.pseudo))
 			break
 		}
-		client.conn.Write([]byte("\033[F")) // Erase the input line.
+		// Erase the input line for the client.
+		client.conn.Write([]byte("\033[F"))
+		// Broadcasts the message to all clients indicating the sender.
 		server.broadcastMessage(client, message)
 	}
 }
 
-func (server *Server) broadcastMessage(sender *Client, message string) {
+func (server *Server) broadcastMessage(sender Client, message string) {
+	// Locks the mutex to synchronize access.
 	server.mutex.Lock()
+	// Ensures the mutex is unlocked after the function exits, preventing deadlock.
 	defer server.mutex.Unlock()
-
-	if sender != nil {
+	// If sender has a pseudo, broadcast message to all clients with the sender name.
+	if sender.pseudo != "" {
 		for _, client := range server.clients {
+			// Append the message to the historic.
 			historic = append(historic, Logs{time.Now().Format("2006-01-02 15:04:05"), sender.pseudo, message})
+			// Send formatted message to each client.
 			client.conn.Write([]byte(fmt.Sprintf("[%s][%s]: %s", time.Now().Format("2006-01-02 15:04:05"), sender.pseudo, message)))
 		}
 	} else {
-		// Broadcast message to all clients (used for notifications)
+		// Broadcast message to all clients (used for notifications).
 		for _, client := range server.clients {
 			client.conn.Write([]byte(message))
 		}
@@ -135,17 +140,8 @@ func (server *Server) broadcastMessage(sender *Client, message string) {
 }
 
 func main() {
-	port := ""
-	if len(os.Args) > 2 {
-		fmt.Println("[USAGE]: ./TCPChat $port")
-		return
-	}
-	if len(os.Args) == 1 {
-		port = "8989"
-	} else {
-		port = os.Args[1]
-	}
-	server := New(&Config{
+	port := GetPort()
+	server := New(Config{
 		Host: "localhost",
 		Port: port,
 	})
